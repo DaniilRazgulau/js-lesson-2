6e935efab097ac6091acd66c2651cd83a1c98067
Razgulau Daniil <razgulaudaniil@gmail.com>
2023-09-23 15:27:19 +0300

    added hw2-js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//N1
+
+let name = prompt(`Ваше имя`);
+let age = prompt(`Ваш возраст`);
+let city = prompt(`Город проживания`);
+let phone = prompt(`Ваш телефон`);
+let email = prompt(`Ваш электронный адрес`);
+let company = prompt(`Компания в которой вы работаете`);
+let year = prompt(`В каком году вы родились`);
+document.write(`<h1>Меня зовут ${name}. Мне ${age} лет. Я проживаю в
+городе ${city} и работаю в компании ${company}. Мои контактные данные:
+${phone}, ${email}.<h1>`);
+
+//N2
+
+document.write(`${name} родился в ${year} году`);
+
+//N3
+
